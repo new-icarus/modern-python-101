bcc7664a2e7c8d8ef7bfcbc0c236b4705a95738d
new-icarus <mcdcsr.tiago@gmail.com>
2022-10-28 11:38:18 -0300

    linting with mypy and type hinting

--- 02_Type_System/variables.py
+++ 02_Type_System/variables.py
@@ -13,5 +13,15 @@ print(box)
 # reasignment to different type
 box = 2
 print(box)
-
 # python is dynamically typed
+
+# type hinting
+food: str = "milk"
+print(f"Louis likes {food}.")
+
+# linting with mypy
+
+# food = False
+# print(f"Louis likes {food}.")
+
+# the code above still runs, but vs code will warn the error
