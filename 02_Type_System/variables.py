aff4b63bcf1e16f95236b6ae3834b65bfb62da4a
new-icarus <mcdcsr.tiago@gmail.com>
2022-10-28 11:19:38 -0300

    variables. type inference.

---
+++ 02_Type_System/variables.py
@@ -0,0 +1,15 @@
+# Declaring a new variable called as 'box'
+box = 'ballons'
+print(box)
+
+#reassignment
+box = 'crackers'
+print(box)
+
+#reassignment
+box = 'hats'
+print(box)
+
+#reasignment to different type
+box = 2
+print(box)
